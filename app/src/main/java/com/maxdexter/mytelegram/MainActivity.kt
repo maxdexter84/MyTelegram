10b277996c815fd8d44670c750e6dd55af07d79b
maxdexter <maxdexter@bk.ru>
2020-08-31 11:43:09 +0300

    инициализировал Drawer, Toolbar

--- app/src/main/java/com/maxdexter/mytelegram/MainActivity.kt
+++ app/src/main/java/com/maxdexter/mytelegram/MainActivity.kt
@@ -2,12 +2,67 @@ package com.maxdexter.mytelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.maxdexter.mytelegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+
+
+    }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder().withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Max Sazonov")
+                    .withEmail("maxdexter@bk.ru")
+            )
+            .build()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun initFields() {
+        toolbar = binding.mainToolbar
     }
 }
