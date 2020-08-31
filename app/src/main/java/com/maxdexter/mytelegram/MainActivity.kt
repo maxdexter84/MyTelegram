82c8610096b5f33e39e5508b83a8349c79472121
maxdexter <maxdexter@bk.ru>
2020-08-31 12:34:17 +0300

    добавил все пункты в Drawer

--- app/src/main/java/com/maxdexter/mytelegram/MainActivity.kt
+++ app/src/main/java/com/maxdexter/mytelegram/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.maxdexter.mytelegram
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.maxdexter.mytelegram.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.Drawer.OnDrawerItemClickListener
 import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var context: Context
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawer: Drawer
     private lateinit var header: AccountHeader
@@ -20,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        context = this
 
     }
 
@@ -58,8 +66,51 @@ class MainActivity : AppCompatActivity() {
                 PrimaryDrawerItem().withIdentifier(100)
                     .withIconTintingEnabled(true)
                     .withName("Создать группу")
-                    .withSelectable(false)
-            ).build()
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать секретный чат")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать канал")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Контакты")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Звонки")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Избранное")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName("Настройки")
+                    .withSelectable(false),
+               DividerDrawerItem(),
+                PrimaryDrawerItem().withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName("Пригласить друзей")
+                    .withSelectable(false),
+                PrimaryDrawerItem().withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName("Вопросы о телеграм")
+                    .withSelectable(false),
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(context, "$position", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+
+            }).build()
     }
 
     private fun initFields() {
