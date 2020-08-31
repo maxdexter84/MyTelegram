779b0cc8764b553a03bd4198a1f2522510abd3aa
maxdexter <maxdexter@bk.ru>
2020-08-31 10:52:19 +0300

    добавил все нужные зависимости

---
+++ app/src/main/java/com/maxdexter/mytelegram/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.maxdexter.mytelegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maxdexter.mytelegram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
